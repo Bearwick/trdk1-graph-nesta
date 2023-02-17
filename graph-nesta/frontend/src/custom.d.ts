e5a2bff00460ea716b8d05f1b2824add5b0b3396
Magne Slatsveen <magneslaatsveen@gmail.com>
2023-02-17 11:14:58 +0100

    Added decleration of image files and some dependencies

--- graph-nesta/frontend/src/custom.d.ts
+++ graph-nesta/frontend/src/custom.d.ts
@@ -1,10 +1,24 @@
-declare module '*.svg' {
-    import * as React from 'react';
-  
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<
-      SVGSVGElement
-    > & { title?: string }>;
-  
-    const src: string;
-    export default src;
-  }
+declare module "*.jpg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.png" {
+    const path: string;
+    export default path;
+}
+declare module "*.jpeg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.svg" {
+    const path: string;
+    export default path;
+}
+declare module "*.webp" {
+    const path: string;
+    export default path;
+}
+
+
