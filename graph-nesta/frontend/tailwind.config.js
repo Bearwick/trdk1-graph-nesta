a2b2e956a1ada7a41aaa1dfd7946a2f756f17440
Magne Slatsveen <magneslaatsveen@gmail.com>
2023-02-23 12:20:34 +0100

    Added custom colors to tailwind.config.js

--- graph-nesta/frontend/tailwind.config.js
+++ graph-nesta/frontend/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'backgorund': '#E3EDF5',
+      'text': '#0D264A',
+      'buttonDark': '#0D264A',
+      'buttonLight':'#00527a',
+      'textLight':'#3969AD',
+      'statusGreen':'#2BB728',
+      'statusOrange':'#F0AE2F',
+      'statusRed':'#FF002F'
+    },
     extend: {},
   },
   plugins: [],
