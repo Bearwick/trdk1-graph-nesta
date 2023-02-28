317f49b9fa3d70616f495e93c34d3cecac254c76
matstn <matstn@stud.ntnu.no>
2023-02-28 12:49:50 +0100

    #3 refined styling

--- graph-nesta/frontend/src/components/Header.tsx
+++ graph-nesta/frontend/src/components/Header.tsx
@@ -18,16 +18,19 @@ function Header(){
 
             <Link to="/Hjem" className="col-span-2 text-3xl text-left ml-20 w-48 mt-2"><p className="">Graph Nesta</p></Link>
 
-                 <div className="flex justify-center">
+                 <div className="text-lg">
 
-                    <div onClick={onChange} className="cursor-pointer ml-40 mt-2 z-20">{!show ? <MenuIcon sx={{ fontSize: "2.5rem"}} /> : <CloseIcon sx={{ fontSize: "2.5rem"}}/>}</div>
+                    <div onClick={onChange} className="cursor-pointer mt-2 z-20">{!show ? <MenuIcon sx={{ fontSize: "2.5rem"}} /> : <CloseIcon sx={{ fontSize: "2.5rem"}}/>}</div>
 
                     {show ?
                         (   
-                            <div className="absolute pt-10 flex flex-col bg-white w-48 border-2 border-black solid" onClick={(e) => {e.stopPropagation()}}>
+                            <div className="bg-white w-1/2 rounded-md ml-8" onClick={(e) => {e.stopPropagation()}}>
                                 {login ? <Link to="/MinProfil"><div className="text-right pr-5 hover:bg-buttonDark hover:text-white">Min profil</div></Link>:<Link to="/LoggInn"><div className="text-right pr-5 hover:bg-buttonDark hover:text-white">Logg inn</div></Link>}
+                                <hr />
                                 <Link to="/MineUtfordringer"><div className="text-right pr-5 hover:bg-buttonDark hover:text-white">Mine utfordringer</div></Link>
+                                <hr />
                                 <Link to="/NyUtfordring"><div className="text-right pr-5 hover:bg-buttonDark hover:text-white">Ny utfordring</div></Link>
+                                <hr />
                                 <Link to="/Søk"><div className="text-right pr-5 hover:bg-buttonDark hover:text-white">Søk</div></Link>
                             </div>
                         ):
