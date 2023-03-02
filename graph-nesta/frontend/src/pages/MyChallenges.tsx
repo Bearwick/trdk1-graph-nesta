f9ee11773bc20b318b2a40c419949bac49a8c76a
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2023-03-02 15:39:03 +0100

    Added Footer

--- graph-nesta/frontend/src/pages/MyChallenges.tsx
+++ graph-nesta/frontend/src/pages/MyChallenges.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ChallengeCard from "../components/ChallengeCard";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function MyChallenges() {
@@ -14,22 +15,20 @@ const handleChallengeShow = ( value: boolean) =>{
       <div className="App">
           <Header />
               
-              <div className="flex flex-col items-center">
-              <div className="flex flex-row text-center gap-1 mt-10">
-                <div onClick={() => {handleChallengeShow(true)}} className={isMyChallenges ? "bg-buttonLight text-lg border-y-4 border-l-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]" : "bg-buttonDark border-y-4 border-l-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]"}>Mine utfordringer</div>
-                <div onClick={() => {handleChallengeShow(false)}} className={!isMyChallenges ? "bg-buttonLight text-lg text-bold border-y-4 border-r-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]" : "bg-buttonDark border-y-4 border-r-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]"}>Abbonerte utfordringer</div>
-              </div>
-              </div>
-
-          <div className="flex flex-wrap justify-center overflow gap-4 mt-16">
-                  <ChallengeCard/>
-                  <ChallengeCard/>
-                  <ChallengeCard/>
-                  <ChallengeCard/>
-                  <ChallengeCard/>
-                  <ChallengeCard/>
-
+          <div className="flex flex-col items-center min-h-[82vh]">
+            <div className="flex flex-row text-center gap-1 mt-10">
+              <div onClick={() => {handleChallengeShow(true)}} className={isMyChallenges ? "bg-buttonLight text-lg border-y-4 border-l-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]" : "bg-buttonDark border-y-4 border-l-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]"}>Mine utfordringer</div>
+              <div onClick={() => {handleChallengeShow(false)}} className={!isMyChallenges ? "bg-buttonLight text-lg text-bold border-y-4 border-r-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]" : "bg-buttonDark border-y-4 border-r-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]"}>Abbonerte utfordringer</div>
+             </div>
+  
+            <div className="flex flex-wrap justify-center overflow gap-4 mt-16 mb-5">
+              <ChallengeCard/>
+              <ChallengeCard/>
+              <ChallengeCard/>
+              <ChallengeCard/>
+            </div>
           </div>
+          <Footer />
       </div>
     )
   }
