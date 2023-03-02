ea2ad0a08f2720fa542bbcf86f506cd72ee111ae
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2023-03-02 15:33:39 +0100

    Small visual changes

--- graph-nesta/frontend/src/pages/MyChallenges.tsx
+++ graph-nesta/frontend/src/pages/MyChallenges.tsx
@@ -5,22 +5,22 @@ import Header from "../components/Header";
 function MyChallenges() {
 
 const[isMyChallenges, setIsMyChallenges] = useState(true)
-const onChange = () =>{
-  setIsMyChallenges(!isMyChallenges);
+
+const handleChallengeShow = ( value: boolean) =>{
+  setIsMyChallenges(value);
 }
 
     return (
       <div className="App">
           <Header />
-          <header className="flex flex-row">
-              <div className="grid grid-cols-11 mx-auto text-center w-2/5 mt-10">
-                {isMyChallenges ? 
-                  <h1 className="text-2xl font-bold underline col-span-5">Mine utfordringer</h1> : <h1 onClick={onChange} className="col-span-5 text-xl text-slate-400 opacity-70 cursor-pointer">Mine utfordringer</h1>}
-                <p className="mx-auto text-2xl">|</p>
-                {!isMyChallenges ? 
-                  <h1 className="text-2xl font-bold underline col-span-5">Du har samme problem</h1> : <h1 onClick={onChange} className="col-span-5 text-xl text-slate-400 opacity-70 cursor-pointer">Du har samme problem</h1>}
+              
+              <div className="flex flex-col items-center">
+              <div className="flex flex-row text-center gap-1 mt-10">
+                <div onClick={() => {handleChallengeShow(true)}} className={isMyChallenges ? "bg-buttonLight text-lg border-y-4 border-l-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]" : "bg-buttonDark border-y-4 border-l-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]"}>Mine utfordringer</div>
+                <div onClick={() => {handleChallengeShow(false)}} className={!isMyChallenges ? "bg-buttonLight text-lg text-bold border-y-4 border-r-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]" : "bg-buttonDark border-y-4 border-r-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]"}>Abbonerte utfordringer</div>
               </div>
-          </header>
+              </div>
+
           <div className="flex flex-wrap justify-center overflow gap-4 mt-16">
                   <ChallengeCard/>
                   <ChallengeCard/>
