e6481ca69004dd5fac1c86a8f731ffa9d5c28b53
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2023-03-07 11:28:13 +0100

    Resolve "Create login register page"

--- graph-nesta/frontend/src/components/Header.tsx
+++ graph-nesta/frontend/src/components/Header.tsx
@@ -14,17 +14,17 @@ function Header(){
 
     return(
         
-        <div className="h-14 grid grid-cols-2 gap-4 place-content-between bg-white sticky top-0 z-50 drop-shadow-xl">
+        <div className="h-14 grid grid-cols-2 gap-4 place-content-between bg-white sticky top-0 z-50 drop-shadow-xl pl-20 pr-10">
 
-            <Link to="/Hjem" className="text-3xl text-left ml-20  w-48 mt-2"><p className="">Graph Nesta</p></Link>
+            <Link to="/Hjem" className="text-3xl text-left w-48 mt-2"><p className="">Graph Nesta</p></Link>
 
             <div className="text-base">
 
                     {show ?
                         (   
                             <div className="flex flex-col items-end" >
-                            <div  className="mt-2 mr-[-2rem]  z-20 "> <CloseIcon onClick={onChange} sx={{ fontSize: "2.5rem", cursor: "pointer"}}/>
-                            <div className="bg-menuBackground w-36 mr-[7rem] rounded-b mt-2 z-20">
+                            <div  className="mt-2 z-20 "> <CloseIcon onClick={onChange} sx={{ fontSize: "2.5rem", cursor: "pointer"}}/>
+                            <div className="bg-menuBackground w-36 ml-[-6.5rem]  rounded-b mt-2 z-20">
                                 <hr />
                                 {login ? <Link to="/MinProfil"><div className="text-right pr-1 hover:bg-buttonDark hover:text-white">Min profil</div></Link>:<Link to="/LoggInn"><div className="text-right pr-1 hover:bg-buttonDark hover:text-white">Logg inn</div></Link>}
                                 <hr />
@@ -40,7 +40,7 @@ function Header(){
                        
                         (
                             <div className="flex flex-col items-end" >
-                            <div className="mt-2 z-20 mr-[5rem] "> 
+                            <div className="mt-2 z-20 "> 
                             <MenuIcon onClick={onChange}  sx={{ fontSize: "2.5rem", cursor: "pointer"}} />
                             </div>
                             </div>
