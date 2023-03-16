6aa40d6b858f9c2b3186a83e67e5118f982c91af
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2023-03-16 13:36:13 +0100

    Fixed norwegian spelling error MyChallenges.tsx

--- graph-nesta/frontend/src/pages/MyChallenges.tsx
+++ graph-nesta/frontend/src/pages/MyChallenges.tsx
@@ -18,7 +18,7 @@ const handleChallengeShow = ( value: boolean) =>{
           <div className="flex flex-col items-center min-h-[82vh]">
             <div className="flex flex-row text-center gap-1 mt-10">
               <div onClick={() => {handleChallengeShow(true)}} className={isMyChallenges ? "bg-buttonLight text-lg border-y-4 border-l-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]" : "bg-buttonDark border-y-4 border-l-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-l-3xl w-[245px]"}>Mine utfordringer</div>
-              <div onClick={() => {handleChallengeShow(false)}} className={!isMyChallenges ? "bg-buttonLight text-lg text-bold border-y-4 border-r-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]" : "bg-buttonDark border-y-4 border-r-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]"}>Abbonerte utfordringer</div>
+              <div onClick={() => {handleChallengeShow(false)}} className={!isMyChallenges ? "bg-buttonLight text-lg text-bold border-y-4 border-r-4 border-buttonLight cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]" : "bg-buttonDark border-y-4 border-r-4 border-buttonDark cursor-pointer text-white py-2 px-5 rounded-r-3xl w-[245px]"}>Abonnerte utfordringer</div>
              </div>
   
             <div className="flex flex-wrap justify-center overflow gap-4 mt-16 mb-5">
