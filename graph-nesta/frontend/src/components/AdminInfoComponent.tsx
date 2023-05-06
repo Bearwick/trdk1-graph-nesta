00509d625caa02b3a3eb3bfe22e7cddab313e9a3
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2023-05-06 11:47:33 +0200

    Resolve "admin panel row width"

--- graph-nesta/frontend/src/components/AdminInfoComponent.tsx
+++ graph-nesta/frontend/src/components/AdminInfoComponent.tsx
@@ -22,7 +22,7 @@ function AdminInfoComponent(props: AdminInfo) {
   }
 
   return (
-    <div className="bg-buttonDark h-24 sm:h-32 min-w-[8rem] sm:min-w-[14rem] text-white whitespace-nowrap px-5 py-2 sm:gap-2 flex flex-col cursor-pointer hover:drop-shadow-3xl hover:bg-buttonHover">
+    <div className="bg-buttonDark h-24 sm:h-32 w-[10rem] sm:w-[14rem] text-white whitespace-nowrap px-2 sm:px-5 py-2 sm:gap-2 flex flex-col cursor-pointer hover:drop-shadow-3xl hover:bg-buttonHover">
       <Link to={props.link} onClick={handleLocalStorage}>
         <h1 className="text-lg sm:text-2xl">{props.title}</h1>
         <p className="text-3xl sm:text-5xl">{props.count}</p>
