873be432ed769dadf08c9daf90c2b23f7f12e1cc
Silviu Catalin Mihai <silviucm@stud.ntnu.no>
2023-05-08 12:22:50 +0200

    bug fix

--- graph-nesta/frontend/cypress/e2e/Interactions.cy.ts
+++ graph-nesta/frontend/cypress/e2e/Interactions.cy.ts
@@ -3,27 +3,27 @@ describe('Test the different interaction between users and problems', () => {
     cy.visit('http://localhost:3000/Hjem')
     cy.get('[data-cy="hamburgerMenu"]').click()
     cy.get('[data-cy="loggin"]').click()
-    cy.get('[data-cy="email"]').type('exampleUser@Malvik.kommune.no')
-    cy.get('[data-cy="password"]').type('1234')
+    cy.get('[data-cy="email"]').type('bruker@malvik.kommune.no')
+    cy.get('[data-cy="password"]').type('bruker')
     cy.get('[data-cy="logginButton"]').click()
 
   })
   after( () => {
     cy.visit('http://localhost:3000/Hjem')
     cy.get('[data-cy="search"]').click()
-    cy.contains("barnehage").click()
+    cy.contains("Lønnsoppgjør").click()
     cy.get('[data-cy = "subscribeBtn"]').click()
   })
 
   it('Checks that it is possible to subscribe to a problem', () => {
     cy.get('[data-cy="search"]').click()
-    cy.contains("barnehage").click()
+    cy.contains("Lønnsoppgjør").click()
     cy.get('[data-cy = "subscribeBtn"]').click()
     cy.wait(2000)
     cy.get('[data-cy = "subcount"]').contains("1")
-    cy.contains("exampleUser@Malvik.kommune.no")
+    cy.contains("bruker@malvik.kommune.no")
     cy.contains("Malvik kommune")
-    cy.contains("45596332")
+    cy.contains("12345678")
 
   })
 
@@ -31,7 +31,7 @@ describe('Test the different interaction between users and problems', () => {
     cy.get('[data-cy="hamburgerMenu"]').click()
     cy.get('[data-cy="myProblems"]').click()
     cy.get('[data-cy="subscribedProblems"]').click()
-    cy.contains("barnehage")
+    cy.contains("Lønnsoppgjør")
   })
   it("Checks that the profile information is right", () => {
     cy.get('[data-cy="hamburgerMenu"]').click()
@@ -39,7 +39,7 @@ describe('Test the different interaction between users and problems', () => {
     cy.get('[data-cy="profileInfo"]').should("be.visible")
     .and("contain", "Kontaktinformasjon")
     .and("contain", "Malvik kommune")
-    .and("contain", "exampleUser@Malvik.kommune.no")
-    .and("contain", "45596332")
+    .and("contain", "bruker@malvik.kommune.no")
+    .and("contain", "12345678")
   })
 })
